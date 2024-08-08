7e3664b0ea8bcdf43c833f558caf88980cacae1e
Georgy Saukov <georgy.saukov@sap.com>
2024-08-08 19:07:16 +0200

    slider base setup

--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import Splide from '@splidejs/splide';
 
 @Component({
   selector: 'app-slides',
   templateUrl: './slides.component.html',
   styleUrl: './slides.component.css'
 })
-export class SlidesComponent {
+export class SlidesComponent implements AfterViewInit {
+  @ViewChild('splideComponent') splideComponent!: ElementRef;
+
+  splide!: Splide;
+
+  ngAfterViewInit() {
+
+  }
 
 }
