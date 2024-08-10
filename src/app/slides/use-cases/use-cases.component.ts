50bf5f60945b751eca16460b84df4b23621faebc
Georgy Saukov <georgy.saukov@sap.com>
2024-08-10 21:15:18 +0200

    use cases slides

---
+++ src/app/slides/use-cases/use-cases.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-use-cases',
+  templateUrl: './use-cases.component.html',
+  styleUrl: './use-cases.component.scss'
+})
+export class UseCasesComponent {
+
+}
