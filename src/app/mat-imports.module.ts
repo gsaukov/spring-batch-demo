32de9f75d4871e56a4f9de0371a4f1b8cf514725
Georgy Saukov <georgy.saukov@sap.com>
2024-08-10 21:10:09 +0200

    mat imports

---
+++ src/app/mat-imports.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core"
+import {MatCardModule} from "@angular/material/card"
+
+@NgModule({
+  imports: [
+    MatCardModule,
+  ],
+  exports:[
+    MatCardModule,
+  ],
+  providers: []
+})
+export class MatImportsModule { }
