859d99d7fed7fc23756ca77d4be3989692f66edf
Georgy Saukov <georgy.saukov@sap.com>
2024-08-11 21:00:19 +0200

    scaling slides

---
+++ src/app/slides/scaling/scaling.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scaling',
+  templateUrl: './scaling.component.html',
+  styleUrl: './scaling.component.scss'
+})
+export class ScalingComponent {
+
+}
