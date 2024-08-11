25f068ccbb213dc28b75fec35045f83f6615a3c9
Georgy Saukov <georgy.saukov@sap.com>
2024-08-11 21:07:08 +0200

    modules integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,47 @@ import { HttpClientModule } from '@angular/common/http'
 import { RouterOutlet } from '@angular/router'
 import { NgxSplideModule } from 'ngx-splide'
 import {SlidesComponent} from "./slides/slides.component";
+import {NavigationBlockedComponent} from "./navigation/navigation-blocked/navigation-blocked.component";
+import {PageNotFoundComponent} from "./navigation/page-not-found/page-not-found.component";
+import {MatImportsModule} from "./mat-imports.module";
+import {CounterDirective} from "./navigation/page-not-found/counter.directive";
+import {AppRoutingModule} from "./app-routing.module";
+import { IntroComponent } from './slides/intro/intro.component';
+import { HistoryComponent } from './slides/history/history.component';
+import { UseCasesComponent } from './slides/use-cases/use-cases.component';
+import { ArchitectureComponent } from './slides/architecture/architecture.component';
+import { DataFlowComponent } from './slides/data-flow/data-flow.component';
+import { ScalingComponent } from './slides/scaling/scaling.component';
+import { ImplementationComponent } from './slides/implementation/implementation.component';
+import { IntegrationsComponent } from './slides/integrations/integrations.component';
+import { ClassicSetupComponent } from './slides/classic-setup/classic-setup.component';
+import { BenefitsComponent } from './slides/benefits/benefits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlidesComponent
+    SlidesComponent,
+    NavigationBlockedComponent,
+    PageNotFoundComponent,
+    CounterDirective,
+    IntroComponent,
+    HistoryComponent,
+    UseCasesComponent,
+    ArchitectureComponent,
+    DataFlowComponent,
+    ScalingComponent,
+    ImplementationComponent,
+    IntegrationsComponent,
+    ClassicSetupComponent,
+    BenefitsComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterOutlet,
-    NgxSplideModule
+    NgxSplideModule,
+    MatImportsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
@@ -25,5 +55,4 @@ import {SlidesComponent} from "./slides/slides.component";
 export class AppModule {
   constructor() {
   }
-
 }
