3bebc08ff19f75152b0a6a51737f18c62141bdfc
Georgy Saukov <georgy.saukov@sap.com>
2024-08-11 21:03:17 +0200

    integrations slides

---
+++ src/app/slides/integrations/integrations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-integrations',
+  templateUrl: './integrations.component.html',
+  styleUrl: './integrations.component.scss'
+})
+export class IntegrationsComponent {
+
+}
